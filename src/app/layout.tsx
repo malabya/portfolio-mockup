1d598503a0400cb7e1592104d43858a502cd060f
Malabya <malabya35@gmail.com>
2025-05-30 19:48:44 +0530

    Added custom fonts.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,35 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFonts from "next/font/local";
 import "./globals.css";
 
+const metroFont = localFonts({
+  src: "../../public/fonts/metropolis/Metropolis-Bold.woff2",
+  variable: "--font-metropolis",
+})
+
+const workFont = localFonts({
+  src: [
+    {
+      path: '../../public/fonts/worksans/worksans-regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/worksans/worksans-bold.woff',
+      weight: '800',
+      style: 'italic',
+    }
+  ],
+  variable: "--font-work-sans",
+})
+
 export const metadata: Metadata = {
-  title: "Portfolio Mockup - Malabya"
+  title: "Portfolio Mockup"
 };
 
-export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
+export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${metroFont.variable} ${workFont.variable}`}>
       <body className="text-black antialiased">
         {children}
       </body>
