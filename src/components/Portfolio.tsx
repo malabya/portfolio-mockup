ce2d23736908acf263b2945a1c47611ace956b90
Malabya <malabya35@gmail.com>
2025-06-02 17:25:36 +0530

    Updated portfolio.

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -14,6 +14,13 @@ export default function Portfolio() {
       </p>
       <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 border-t border-gray-200 pt-6 mt-6 sm:pt-12 md:mt-12">
         <ProjectCard
+          name="myStipendium.de"
+          description="Efficiently productivate customized human capital through resource maximizing materials. Compellingly negotiate."
+          image_url="/images/project7.png"
+          href="https://www.pfp-consortium.org/"
+          tags={['Drupal']}
+        />
+        <ProjectCard
           name="Partnership of Peace Consortium"
           description="Efficiently productivate customized human capital through resource maximizing materials. Compellingly negotiate."
           image_url="/images/project1.png"
@@ -37,15 +44,7 @@ export default function Portfolio() {
           tags={['Drupal']}
         />
         <ProjectCard
-          name="Stelar - EDC"
-          description="Efficiently productivate customized human capital through resource maximizing materials. Compellingly negotiate."
-          image_url="/images/project4.png"
-          owner="Specbee"
-          href="https://www.pfp-consortium.org/"
-          tags={['Drupal']}
-        />
-        <ProjectCard
-          name="Stelar - EDC"
+          name="Centre for Professional Education"
           description="Efficiently productivate customized human capital through resource maximizing materials. Compellingly negotiate."
           image_url="/images/project5.png"
           owner="Specbee"
@@ -53,7 +52,7 @@ export default function Portfolio() {
           tags={['Drupal']}
         />
         <ProjectCard
-          name="Stelar - EDC"
+          name="Curtiss Wright - Defense Solutions"
           description="Efficiently productivate customized human capital through resource maximizing materials. Compellingly negotiate."
           image_url="/images/project6.png"
           owner="Specbee"
